7173eca4d8436762e8aeced2e448656a8e10d977
Anil Kumar Yadav <anilkumar.yadav@cognologix.com>
2024-11-26 14:53:01 +0530

    use deployed url of backend

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,4 +1,4 @@
-// let baseUrl = "https://expanse-tracker-backend-e193.onrender.com";
-let baseUrl = "http://192.168.194.199:5000";
+let baseUrl = "https://expanse-tracker-backend-e193.onrender.com";
+// let baseUrl = "http://192.168.194.199:5000";
 
 export default baseUrl;
